77081f37dff3fb7c7bd76147e7987b1137ffdbf6
XXXXXX <XXXXX@example.com>
2022-12-26 22:46:28 +0900

    user add model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  get  '/signup',  to: 'users#new'
   get '/help', to: 'static_pages#help',  as:'helf'
   get '/about', to: 'static_pages#about'
+  root 'static_pages#home'
 #  get 'static_pages/about'
 
 end
---
+++ db/migrate/20221226125039_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.string :password_digest
+      t.string :remember_digest
+      t.integer :emp_no
+      t.boolean :admin, default: false
+      t.date :join_date
+      t.integer :committee_id
+      t.integer :yakushoku_id
+      t.string :liveaddress
+      t.string :station
+      t.bigint :salary
+      t.date :bh_date
+  
+
+      t.timestamps
+    end
+  end
+end
