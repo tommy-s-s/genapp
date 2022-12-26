79efad34a7d475c63c398e4cfb2e62dd421f23d0
XXXXXX <XXXXX@example.com>
2022-12-26 19:10:45 +0900

    user add before

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help',  as:'helf'
+  get '/about', to: 'static_pages#about'
+#  get 'static_pages/about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
